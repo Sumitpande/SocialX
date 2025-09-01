4d40bc68d9288edd1226d56859d99c0657e32936
Sumit <sumitakashpande1230@gmail.com>
2025-09-01 22:16:04 +0530

    changed emoji picker

--- client/src/components/messages/EmojiPicker.tsx
+++ client/src/components/messages/EmojiPicker.tsx
@@ -1,12 +1,11 @@
 import {
   Popover,
   PopoverContent,
-  PopoverTrigger
+  PopoverTrigger,
 } from "@/components/ui/popover";
 import { SmileIcon } from "lucide-react";
-import Picker from "@emoji-mart/react";
-import data from "@emoji-mart/data";
-
+import { lazy } from "react";
+const LazyEmojiPicker = lazy(() => import("emoji-picker-react"));
 interface EmojiPickerProps {
   onChange: (value: string) => void;
 }
@@ -18,12 +17,12 @@ export const EmojiPicker = ({ onChange }: EmojiPickerProps) => {
         <SmileIcon className="h-5 w-5 text-muted-foreground hover:text-foreground transition" />
       </PopoverTrigger>
       <PopoverContent className="w-full">
-        <Picker
-          emojiSize={18}
-          theme="light"
-          data={data}
-          maxFrequentRows={1}
-          onEmojiSelect={(emoji: any) => onChange(emoji.native)}
+        <LazyEmojiPicker
+          open={true}
+          onEmojiClick={(emoji) => onChange(emoji.emoji)}
+          searchDisabled={false}
+          height={400}
+          width={300}
         />
       </PopoverContent>
     </Popover>
